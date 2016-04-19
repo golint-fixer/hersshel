91b90f3317ec6f7f48470fcf2f5d8d6dd8bbe2f3
Guillaume Fillon <guillaume@posteo.de>
2016-04-18 19:35:51 -0700

    Add Hersshel CLI into cmd package

--- cmd/hersshel/main.go
+++ cmd/hersshel/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/gin-gonic/contrib/ginrus"
+	"github.com/hersshel/hersshel/config"
+	"github.com/hersshel/hersshel/router"
+	"github.com/hersshel/hersshel/router/middleware"
+)
+
 func main() {
-	return
+	var cfg *config.Config
+
+	cfg = config.LoadConfigFromEnv()
+	if cfg.Server != nil && cfg.Server.GOMAXPROCS != nil {
+		runtime.GOMAXPROCS(*cfg.Server.GOMAXPROCS)
+	} else {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
+	handler := router.Load(
+		ginrus.Ginrus(logrus.StandardLogger(), time.RFC3339, true),
+		middleware.Store(cfg.PostgreSQL),
+	)
+
+	logrus.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Server.HTTPPort), handler))
 }
